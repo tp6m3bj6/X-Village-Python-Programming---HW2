9f9038fc7742fe838c94bcdec105ea63b82fbe31
Derek,Chen <you@example.com>
2018-07-27 01:12:51 +0800

    X-Village-Python-Programming-HW2

--- HW2.py
+++ HW2.py
@@ -19,40 +19,52 @@ data2=json.dumps(data, ensure_ascii=False, indent=4)
 
 ###擷取資料
 x=[]
-list1=[]
-list2=[]
-list3=[]
-list4=[]
-list5=[]
+y1=[]
+y2=[]
+y3=[]
+y4=[]
+y5=[]
+y6=[]
+y7=[]
+y8=[]
+y9=[]
+y10=[]
+y11=[]
+
 for i in data:
-    list1.append(float(i['工業用電(百萬度)']))
-    list2.append(float(i['民生用電(百萬度)']))
-    list3.append(float(i['尖峰負載(MW)']))
-    list4.append(float(i['備轉容量(MW)']))
-    list5.append(float(i['備轉容量率(%)']))
+    y1.append(float(i['工業用電(百萬度)']))
+    y2.append(float(i['民生用電(百萬度)']))
+    y3.append(float(i['尖峰負載(MW)']))
+    y4.append(float(i['備轉容量(MW)']))
+    y5.append(float(i['備轉容量率(%)']))
+    y6.append(float(i['核一#1(萬瓩)']))
+    y7.append(float(i['核一#2(萬瓩)']))
+    y8.append(float(i['核二#1(萬瓩)']))
+    y9.append(float(i['核二#2(萬瓩)']))
+    y10.append(float(i['核三#1']))
+    y11.append(float(i['核三#2']))
+
 
 ###繪製工業用電與民生用電圓餅圖
-y1=sum(list1)
-y2=sum(list2)
+y1=sum(y1)
+y2=sum(y2)
 labels='For Industy use(GWh)','For General use(GWh)'
 y=(y1,y2)
-
-##plot圓餅圖，比較工業用電和民生用電
 plt.pie(y , labels = labels,autopct='%1.1f%%')
 plt.show()
 
 ###雙Ｙ軸圖表，比較尖峰負載以及備載容量
-x=np.arange(0,len(list3),1)
+x=np.arange(0,len(y3),1)
 fig = plt.figure()
 
 ax1 = fig.add_subplot(111)
-l1, =ax1.plot(x, list3, label='Peaking power(MW)', color='blue')
+l1, =ax1.plot(x, y3, color='blue')
 ax1.set_ylabel('Peaking power(MW)')
 ax1.set_title("106/01/01-107/06/30")
 ax1.set_xlabel('Day')
 
 ax2 = ax1.twinx()  
-l2, =ax2.plot(x, list4, 'r',label='Operating reserve(MW)', color='red')
+l2, =ax2.plot(x, y4, 'r', color='red')
 ax2.set_xlim([0, len(x)])
 ax2.set_ylabel('Operating reserve(MW)')
 ax2.set_ylim([500, 10000])
@@ -61,6 +73,21 @@ ax2.set_ylim([500, 10000])
 plt.legend(handles = [l1, l2,], labels = ['Peaking power(MW)', 'Operating reserve(MW)'], loc = 'best')
 plt.show()
 
-###
+###統計各核能場總發電量
+yt=sum(y6),sum(y7),sum(y8),sum(y9),sum(y10),sum(y11)
+index = np.arange(1,7)
+plt.bar(index,height =yt, width=0.35, alpha=0.4)
+plt.xlabel('Nuclear Plant')
+plt.ylabel('KW')
+plt.xticks(index,('NP1#1','NP1#2','NP2#1','NP2#2','NP3#1','NP3#2'))
+plt.show()
 
+###統計核能場佔總發電量百分比
+ytotal=sum(y)
+ytotal_N=sum(yt)
+labels='Traditional','Newclear'
+y_present=(ytotal,ytotal_N)
 
+##plot圓餅圖，比較工業用電和民生用電
+plt.pie(y_present , labels = labels,autopct='%1.1f%%')
+plt.show()
